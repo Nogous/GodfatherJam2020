bb1b146eea8bb8fa7fcecee081e2d1c6e04b4016
Camille <camille.lavarde@gmail.com>
2020-09-01 22:31:33 +0200

    basic move

--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerEntity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 1f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    public float offsetAngle = 0f;
+
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private void Update()
     {
-        
+        SetMovement();
+
+        SetOrientation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        DoActionMove();
+
+        Orientate();
     }
+
+    #region Movement
+    private void SetMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void DoActionMove()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    #endregion
+
+    #region Orientation
+    private void SetOrientation()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void Orientate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - offsetAngle;
+        rb.rotation = angle;
+    }
+    #endregion
 }
