03c7bf68629e8dd13cbf20209df4b109d21cf618
Camille <camille.lavarde@gmail.com>
2020-09-02 20:53:10 +0200

    pool spawn object

---
+++ Assets/Scripts/Spawner/TwitteSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwitteSpawner : MonoBehaviour
+{
+    ObjectPooler objectPooler;
+
+    private void Start()
+    {
+        if (ObjectPooler.Instance != null)
+        {
+            objectPooler = ObjectPooler.Instance;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        objectPooler.SpawnFromPool("Twitte", transform.position, Quaternion.identity);
+    }
+}
