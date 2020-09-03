7d06f02a3d70f472b66c4efb4f941d23e634d624
Camille <camille.lavarde@gmail.com>
2020-09-03 18:00:45 +0200

    json

--- Assets/Scripts/GetGoogleShit.cs
+++ Assets/Scripts/GetGoogleShit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,7 +18,6 @@ public class GetGoogleShit : MonoBehaviour
     // https://docs.google.com/spreadsheets/d/1SKBWUcdgmPfcCn9k0BZapoHY8JdBOi_0t8OTA4drjRg/edit?usp=sharing
 
     // tmp https://docs.google.com/spreadsheets/d/1cK1PGkBKDU-Fo6dLv8e1onFvy0m02ja3UFQIgcwgJgA/edit?usp=sharing
-    //     https://docs.google.com/spreadsheets/d/1cK1PGkBKDU-Fo6dLv8e1onFvy0m02ja3UFQIgcwgJgA/edit?usp=sharing
 
     public string documentId = "1SKBWUcdgmPfcCn9k0BZapoHY8JdBOi_0t8OTA4drjRg";
     public string idleModeSheetName = "Donald_Trump";
@@ -30,7 +30,7 @@ public class GetGoogleShit : MonoBehaviour
 
     public void Start()
     {
-        //LoadSheet();
+        LoadSheet();
     }
 
     [ContextMenu("GetShit")]
@@ -38,7 +38,6 @@ public class GetGoogleShit : MonoBehaviour
     {
         StartCoroutine("GetShit");
     }
-
     IEnumerator GetShit()
     {
         string url = "https://sheets.googleapis.com/v4/spreadsheets/" + documentId + "/values/" + idleModeSheetName + "!" + idleModeCellRange.startCell + ":" + idleModeCellRange.endCell + "?majorDimension=ROWS&key=" + apiKey;
@@ -49,8 +48,6 @@ public class GetGoogleShit : MonoBehaviour
         request.method = UnityWebRequest.kHttpVerbGET;
         request.SetRequestHeader("content-type", "application/json; charset=utf-8");
         request.SetRequestHeader("accept", "application/json;");
-
-        GameObject obj = new GameObject();
 
         yield return request.SendWebRequest();
 
@@ -64,19 +61,28 @@ public class GetGoogleShit : MonoBehaviour
         }
         else
         {
+            #region test
+
+            string firstSelection = request.downloadHandler.text.Substring(0,5);
+            int index = firstSelection.IndexOf('=');
+            Debug.Log(index);
+
+            #endregion
+
             Debug.Log("Ok: " + request.responseCode);
             try
             {
                 resultJson = request.downloadHandler.text;
 
-                Dictionary<string, string> loadedData = JsonUtility.FromJson<Dictionary<string, string>>(resultJson);
+                //Dictionary<string, string> loadedData = JsonUtility.FromJson<Dictionary<string, string>>(resultJson);
 
-                Debug.Log(loadedData.Count);
+                //GoogleSheetData data = new GoogleSheetData();
+                //data.values = new string[10, 10];
 
-                foreach (KeyValuePair<string, string> kvp in loadedData)
-                {
-                    Debug.LogFormat("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                }
+                //string json = JsonUtility.ToJson(data);
+                //Debug.Log(json);
+
+                SheetData sheetData = JsonConvert.DeserializeObject<SheetData>(resultJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
                 //result = JsonConvert.DeserializeObject<T>(request.downloadHandler.text, defaultJsonSerializationSetting);
 
@@ -89,5 +95,46 @@ public class GetGoogleShit : MonoBehaviour
         }
     }
 
-     
+    private const string k_googleSheetDocID = "";
+    private const string purl = "https://docs.google.com/d/" + k_googleSheetDocID+"?export?format=csv";
+
+    /*
+    internal static IEnumerator DownloadData(System.Action<string> onCompleted)
+    {
+        yield return new WaitForEndOfFrame();
+
+        string downloadData = null;
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(purl))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Download Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Download success");
+                Debug.Log("Data: " + webRequest.downloadHandler.text);
+
+                string versionSelection = webRequest.downloadHandler.text.Substring(0, 5);
+                int equalsIndex = versionSelection.IndexOf('=');
+                UnityEngine.Assertions.Assert.IsFalse(equalsIndex == -1, "Could not found '='");
+
+                string versionText = webRequest.downloadHandler.text.Substring(0, equalsIndex);
+                Debug.Log("Downloaded data version " + versionText);
+
+                //downloadData = webRequest.downloadHandler.text.Substring(equalsInedx + 1);
+            }
+        }
+    }
+    */
+}
+
+[System.Serializable]
+public class SheetData
+{
+    public string range;
+    public string majorDimension;
+    public List<List<string>> values;
 }
